68de72dc7d76b78f6a204d2574d61f6ae35e1a88
J-Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2019-09-02 06:27:40 +0000

    with Python3

---
+++ Python/11021.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+result = []
+
+for a in range(0, num):
+    a,b = input().split(' ')
+    result.append(int(a) + int(b))
+
+for b in range(0, num):
+    print("Case #"+str(b+1)+':',result[b])
