f1a2844264f2bf717afb4a30577a45637e9e858d
J-Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2019-09-09 13:12:57 +0900

    with Python3

---
+++ Python/1152.py
@@ -0,0 +1,12 @@
+io = input().split(' ')
+for a in io:
+    if a == '':
+        io.remove(a)
+    else:
+        pass
+for a in io:
+    if a == '':
+        io.remove(a)
+    else:
+        pass
+print(len(io))
