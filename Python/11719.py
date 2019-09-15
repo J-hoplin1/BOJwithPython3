530db7ae95701bc87d6947e76ea89a84a4a446d8
J-Hoplin <45956041+J-hoplin1@users.noreply.github.com>
2019-09-15 12:29:20 +0900

    with Python3

---
+++ Python/11719.py
@@ -0,0 +1,10 @@
+'''
+Code written by J-Hoplin
+Date of creation : 2019/9/15
+URL : https://www.acmicpc.net/problem/11719
+'''
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
