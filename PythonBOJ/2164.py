9d6f73d78ce80baf46e457f88a2e4050f12e5fe6
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-01-15 19:56:22 +0900

    2164

--- PythonBOJ/2164.py
+++ PythonBOJ/2164.py
@@ -1,55 +1,75 @@
-from typing import Any
-from collections import deque
-import sys
-
 '''
-N장의 카드
-1~N까지의 번호 + 1번 카드가 제일위 N번 카드가 제일 아래
+풀이
 
--> 제일 위에있는 카드를 바닥에 버린 후 그 다음 제일위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
+순서대로 K번째 사람을 제거한다. 한사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해서 나가진다
+이 과정은 N명의 사람이 제거되면 남은 살마들로 이루어진 원을 따라 이과정을 계속해 나간다.
 
--> 결론적으로 1회 deque() 연산 후 2회 deque()연산값을 큐의 맨 뒤로 enque()한다는 소리
+처음에는 1~N번까지의 cards를 초기화해준다
 
-연산 순서 : deque() - deque() - enque()
+이 문제는 Rounded Queue의 원리를 응용하면 풀수 있다
 
-collections 모듈의 deque메소드를 사용해서 풀어보자
+풀이 과정을 다음과 같다
 
-이 알고리즘에서는 Queue Empty, Queue Full 에대한 경우는 생각하지 않는다.
 
+front : 0
+
+1)
+1234567
+
+delete index of : (front + (K-1)) % len(cards) = 2(3) -> K-1을 하는 이유는 리스트에서 실제로 인덱싱을 할때는 0부터 시작하기 떄문이다
+
+front = 2
+
+2)
+124567
+
+delete index of : (front + (K-1)) % len(cards) = 4(6)
+
+front = 4
+
+3)
+12457
+
+delete index of : (front + (K-1)) % len(cards) = 1(2)
+
+front = 1
+
+4)
+1457
+
+delete index of : (front + (K-1)) % len(cards) = 3(7)
+
+front = 3
+
+5)
+145
+
+delete index of : (front + (K-1)) % len(cards) = 2(5)
+
+front = 2
+
+6)
+14
+
+delete index of : (front + (K-1)) % len(cards) = 0(1)
+front = 0
+
+7)
+4
+
+이제 남은 카드는 4 하나이므로 result에 그냥 넣어준다.
 '''
 
+N,K = input().split()
 
-class queue(object):
-
-    def __init__(self,capacity):
-        self.capacity = capacity
-        self.que = deque([],self.capacity)
-
-    def enque(self, data) -> None:
-        self.que.append(data)
-    
-    def deque(self) -> int:
-        return self.que.popleft()
-    
-    def returnDataCount(self) -> int:
-        return len(self.que)
-    
-    def printQueueFront(self) -> int:
-        return print(self.que[0])
-
-    def dump(self):
-        print(*self.que, sep=' - ')
-
-cards = int(sys.stdin.readline())
-q = queue(cards)
-for t in range(1,cards + 1): 
-    q.enque(t)
-loopBoolean = True
-while q.returnDataCount() != 1:
-    q.deque()
-    if q.returnDataCount() == 1: # deque를 한 후에 큐내 데이터가 1개인 경우 break
-        break
-    else:
-        q.enque(q.deque())
-
-q.printQueueFront()
+cards = [t for t in range(1,int(N) + 1)]
+results = []
+front = 0
+while len(cards) != 1:
+    capacity = len(cards)
+    deleteIndex = (front + (int(K)-1)) % capacity
+    results.append(cards[deleteIndex])
+    del cards[deleteIndex]
+    front = deleteIndex
+results = list(map(str,results + cards))
+print('<' + ', '.join(results) + '>')
