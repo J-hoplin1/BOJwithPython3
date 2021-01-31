139ab540803d0227796328d45a3332dc6affd51d
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-01-31 18:42:02 +0900

    Update hashTest.py

--- StudyPS/Python/Hash/hashTest.py
+++ StudyPS/Python/Hash/hashTest.py
@@ -1,7 +1,7 @@
 from enum import Enum
-from hash import ChainedHash
+from a import ChainedHash
 
-option = Enum('option',['추가','삭제','검색','출력','종료'])
+option = Enum('option',['추가','삭제','검색','출력','종료']) # Enum('지정이름',[값들])
 
 hashTable = ChainedHash(13)
 
@@ -14,9 +14,9 @@ def optionSelect() -> 'option':
             return option(select)
 
 while True:
-    option = optionSelect()
+    opt = optionSelect()
 
-    if option == option.추가:
+    if opt == option.추가: # 지정이름.값 형식으로 나오는것을 검사
         key = int(input('추가할 키값 입력하기 : '))
         value = input('추가할 값 입력하기 : ')
         if not hashTable.add(key,value):
@@ -24,14 +24,14 @@ while True:
         else:
             print("추가 성공")
     
-    elif option == option.삭제:
+    elif opt == option.삭제:
         key = int(input('삭제하고자 하는 데이터의 Key값 입력하기 : '))
         if not hashTable.remove(key):
             print("삭제하고자 하는 Key값은 Hash Table에 존재하지 않습니다")
         else:
             print("삭제 완료")
     
-    elif option == option.검색:
+    elif opt == option.검색:
         key = int(input('검색할 키값 입력하기 : '))
         searchData = hashTable.search(key)
         if not searchData:
@@ -39,7 +39,7 @@ while True:
         else:
             print(f'{key} : {searchData}')
     
-    elif option == option.출력:
+    elif opt == option.출력:
         hashTable.dump()
 
     else:
